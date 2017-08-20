96da30afcfff713d32e332a071123e67c5f142f1
Warmist <warmist@gmail.com>
2017-08-20 13:10:23 +0300

    Simplify plug install

--- server2.lua
+++ server2.lua
@@ -7,9 +7,10 @@ local FPS_LIMIT=50
 	TODO:
 		* change game mode should stop the server
 ]]
-
+local is_restart=false
 if args[1]=="-r" and server then
 	server:stop()
+	is_restart=true
 elseif args[1]=='-s' and server then
 	server:stop()
 	return
@@ -26,32 +27,29 @@ if server then
 end
 
 if not server then
-	server=core:server{users=users,port=6666}
+	server=core:server{users=users}
 end
 
-local map=require'hack.scripts.http.map'
-reload'hack.scripts.http.map'
-server:install_plugin(map.plug{})
-
-local spectate=require'hack.scripts.http.spectate'
-reload'hack.scripts.http.spectate'
-server:install_plugin(spectate.plug{})
-
-local messages=require'hack.scripts.http.messages'
-reload'hack.scripts.http.messages'
-server:install_plugin(messages.plug{})
-server.page_vars.message_of_the_day="Experimenting with new server..."
-
-local commands=require'hack.scripts.http.commands'
-reload'hack.scripts.http.commands'
-server:install_plugin(commands.plug{})
+function inst_plug( name, do_reload,args)
+	local plug=require('hack.scripts.http.'.. name)
+	if do_reload then
+		reload('hack.scripts.http.'.. name)
+	end
+	server:install_plugin(plug.plug(args or {}))
+end
+local general_plugs={
+	'map',
+	'messages',
+	'commands',
+}
+for i,v in ipairs(general_plugs) do
+	inst_plug(v,is_restart)
+end
 
 server.page_vars.message_of_the_day="Experimenting with new server..."
 
 if df.global.gametype==df.game_type.DWARF_ARENA or df.global.gametype==df.game_type.ADVENTURE_ARENA then
-	local economy=require'hack.scripts.http.economy'
-	reload'hack.scripts.http.economy'
-	server:install_plugin(economy.plug{})
+	inst_plug('economy',is_restart)
 else
 	error("This mode is not supported")
 end
